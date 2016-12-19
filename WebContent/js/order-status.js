843c8814214441a10d85dff81f475ddcc9aab4e9
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2016-12-19 22:20:26 +0530

    pretty page for sales orders

--- WebContent/js/order-status.js
+++ WebContent/js/order-status.js
@@ -3,12 +3,15 @@
 function getLabelForOrderStatus(n){
     switch(n){
         case 1: // just added (new order) - warning
-            return '<span class="label label-warning">New</span>';
+            return '<span class="label label-default">New</span>';
             break;
         case 2: // Approved - parimary
-            return '<span class="label label-primary">Approved</span>';
+            return '<span class="label label-info">Approved</span>';
             break;
-        case 3: // Completed - success
+        case 3: // Shipped
+            return '<span class="label label-primary">Shipped</span>';
+            break;
+        case 4: // Completed - success
             return '<span class="label label-success">Completed</span>';
             break;
         default:
--- WebContent/users/sales/js/order.js
+++ WebContent/users/sales/js/order.js
@@ -11,13 +11,48 @@ var order = alasql('SELECT * FROM ordersremove WHERE id=?', [orderID])[0];
 
 // put details of order
 $('#order-id').text(orderID);
-$('#customer-id').text(order.customer_id);
+$('#customer-id').text(alasql('select * from customers where id=?',[order.customer_id])[0].name);
 $('#status').empty();
 $('#status').append(getLabelForOrderStatus(order.status));
 
-
+var status = order.status;
+if(status > 1){ //2 or more
+    $('#new-order .line').css('background-color','#4caf50');
+    $('#order-approved .imgcircle').css('background-color','#4caf50');
+    $('#span-2').show();
+    
+    if(status > 2){ //3 or more
+        $('#order-approved .line').css('background-color','#4caf50');
+        $('#order-shipped .imgcircle').css('background-color','#4caf50');
+        $('#span-3').show();
+    }
+    
+    if(status > 3){ //complete
+        $('#order-shipped .line').css('background-color','#4caf50');
+        $('#order-complete .imgcircle').css('background-color','#4caf50');
+        $('#span-4').show();
+    }
+}
 
 var details = alasql('SELECT * FROM ordersremovedetails WHERE order_id=?',[orderID]);
+
+function getPrettyDate(daaate){
+    return moment(daaate).format('LL');
+}
+
+function getShortPrettyDate(daaate){
+    return moment(daaate).format('ll');
+}
+
+// set shipping details
+$('#address').empty();
+$('#address').append('<strong>'+alasql('select * from customers where id=?',[order.customer_id])[0].name+'</strong>');
+$('#address').append('<p>'+alasql('select * from customers where id=?',[order.customer_id])[0].addr+'</p>');
+
+$('#date-1').text(getPrettyDate(order.date_received));
+$('#date-2').text(getPrettyDate(order.date_approved));
+$('#date-3').text(getPrettyDate(order.date_shipped));
+$('#date-4').text(getPrettyDate(order.date_completed));
 
 function populateTable(){
     var tbody_order_details = $('#tbody-sales-order-details');
@@ -26,12 +61,13 @@ function populateTable(){
     for (var i = 0; i < details.length; i++) {
         var detail = details[i];
         var product = products[detail.product_id - 1];
-        co(product);
+        var kind = alasql('select * from kind where id = ?',[product.kind])[0].text;
         var tr = $('<tr></tr>');
         tr.append('<td class="col-md-1">' + detail.id + '</td>');
-        tr.append('<td class="col-md-2">' + product.detail + '</td>');
-
-        tr.append('<td class="col-md-1">' + detail.quantity + '</td>');
+        tr.append('<td class="col-md-3">' + kind + '</td>');
+        tr.append('<td class="col-md-3">' + product.maker + '</td>');
+        tr.append('<td class="col-md-3">' + product.detail + '</td>');
+        tr.append('<td class="col-md-2">' + detail.quantity + '</td>');
         tr.appendTo(tbody_order_details);
     }
 }
