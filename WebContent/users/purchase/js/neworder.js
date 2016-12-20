93c15f61d70f50bee0cde988baf1b18a5e694f92
Man Parvesh Singh Randhawa <randhawa_m@worksap.co.jp>
2016-12-20 17:32:17 +0530

    doing something on new order page of purchase

--- WebContent/users/purchase/js/neworder.js
+++ WebContent/users/purchase/js/neworder.js
@@ -327,15 +327,15 @@ $('#pura-ok').on('click', function(){
         if (rows.length > 0) {
             stock_id = rows[0].id; 
             balance = rows[0].balance;
-            alasql('UPDATE stock SET balance = ? WHERE id = ?', [ balance + qty, stock_id ]);
+            //alasql('UPDATE stock SET balance = ? WHERE id = ?', [ balance + qty, stock_id ]);
         } else {
             stock_id = alasql('SELECT MAX(id) + 1 as id FROM stock')[0].id;
-            alasql('INSERT INTO stock VALUES(?,?,?,?)', [ stock_id, item, whouse, balance + qty ]);
+            //alasql('INSERT INTO stock VALUES(?,?,?,?)', [ stock_id, item, whouse, balance + qty ]);
         }
         // add trans record
         var trans_id = alasql('SELECT MAX(id) + 1 as id FROM trans')[0].id;
         var supplier_id = parseInt($('#row-' + i + '-suppliers').val());
-        alasql('INSERT INTO trans VALUES(?,?,?,?,?,?)', [ trans_id, stock_id, date, qty, balance + qty, memo ]);
+        //alasql('INSERT INTO trans VALUES(?,?,?,?,?,?)', [ trans_id, stock_id, date, qty, balance + qty, memo ]);
         
     }
 	
