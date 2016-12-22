1fdf12ba643cdf3fcee8b3e2b0115a8ef1d58b00
Man Parvesh Singh Randhawa <randhawa_m@worksap.co.jp>
2016-12-22 09:22:26 +0530

    resolve #44

--- WebContent/users/sales/js/index.js
+++ WebContent/users/sales/js/index.js
@@ -125,9 +125,12 @@ function showOrders(n){
             orders = alasql('SELECT * FROM ordersremove WHERE status=?', [2]);
             break;
         case 3:
-            orders = alasql('SELECT * FROM ordersremove WHERE status=?', [4]);
+            orders = alasql('SELECT * FROM ordersremove WHERE status=?', [3]);
             break;
         case 4:
+            orders = alasql('SELECT * FROM ordersremove WHERE status=?', [4]);
+            break;
+        case 5:
             orders = alasql('SELECT * FROM ordersremove');
             break;
     }
